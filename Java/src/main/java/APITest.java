958c560d53e647f7cc150a93c2da859e1ceb9b96
M-Jic111 <marko.jandric02@gmail.com>
2025-11-27 20:40:32 +0100

    cleaned up code and added comments

--- Java/src/main/java/APITest.java
+++ Java/src/main/java/APITest.java
@@ -1,13 +1,17 @@
 import org.eclipse.sumo.libtraci.*;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
-
+//basic connection showcase of sumo using libtraci
 public class APITest {
+
+   //function to either connect to sumo with or without gui
     public static String openguiYN(){
         Scanner input =new Scanner(System.in);
         System.out.println("Do you want to open the Simulation with the Sumo GUI?[Y/N]");
         String answer = input.nextLine().trim().toLowerCase();
+        input.close();
         boolean open_gui = answer.equals("y");
         String sumo;
         if(open_gui){
@@ -19,8 +23,9 @@ public class APITest {
     }
 
     public static void main(String[] args) {
-
-
+        //sets up a decimalformat class instance to use later in the output of car coordinates
+        DecimalFormat cutoffdecimals = new DecimalFormat("#.00");
+        //sets path to sumo files according to user directory
         String base = System.getProperty("user.dir");
 
         String configPath = base + "/Java/src/main/resources/SumoConfig/altstadt.sumocfg";
@@ -29,28 +34,32 @@ public class APITest {
 
 
         Simulation.preloadLibraries();
+        //loading up instance of sumo with given variables
         Simulation.start(new StringVector(new String[]{
                 openguiYN(),
                 "-c", configPath,"-r",routePath
         }));
 
 
-
+        //steps through simulation
         for (int i = 0; i < 1000; i++) {
             Simulation.step();
-
-            System.out.println("Step: " + Simulation.getTime());
+            int cars_amount = Vehicle.getIDCount();
+            System.out.println("Step: " + Simulation.getTime() + " ("+cars_amount+" cars in the Simulation)");
             StringVector cars_list = Vehicle.getIDList();
+
+            //prints position for every vehicle in the simulation
 
             for (String id : cars_list) {
                 TraCIPosition pos = Vehicle.getPosition(id);
-                System.out.println("  " + id + " at (" + pos.getX() + ", " + pos.getY() + ")");
+                System.out.println("  " + id + " at (" + cutoffdecimals.format(pos.getX())  + ", " + cutoffdecimals.format(pos.getY()) + ")");
 
             }}
-
+        //summarizes amount and id of traffic lights at end of simulation
+        int trafficlights_amount = TrafficLight.getIDCount();
         StringVector trafficlights_list =TrafficLight.getIDList();
 
-            System.out.println("Traffic light id: "+ trafficlights_list);
+            System.out.println("This Simulation contained "+trafficlights_amount+ " Traffic lights with the ids: "+ trafficlights_list);
 
 
 
